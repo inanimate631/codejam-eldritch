c91b0c1860188bd087dad1c09e7bab1227b306d6
Artem sssss <inanmate631@gmail.com>
2022-08-29 14:49:41 +0300

    fix: deploy1

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+  minifySvg: false,
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-              },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(js)$/, use: 'babel-loader' }, 
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                  },
-                ],
-              },
-        ]
-    },
-    plugins: [
-        new  HtmlWebPackPlugin({
-          template: './app/index.html',
-          filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-          filename: '[name].css',
-          chunkFilename: '[id].css',
-        }),
-      ],
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './app/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 }
